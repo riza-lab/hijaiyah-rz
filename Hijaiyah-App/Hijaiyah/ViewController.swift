9f4dc46fa77b79f834255df4c160289129221efa
riza-lab <68800472+riza-lab@users.noreply.github.com>
2020-12-10 14:19:06 +0700

    Add files via upload

---
+++ Hijaiyah-App/Hijaiyah/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hijaiyah
+//
+//  Created by Edi Sunardi on 19/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
